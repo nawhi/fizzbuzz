1e5004ff79daee74642aa45f2779608946596fae
nawhi <25394965+nawhi@users.noreply.github.com>
2019-04-02 17:14:41 +0100

    Add Fizz for multiples of 3

--- src/main/java/com/codurance/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/codurance/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,7 @@ package com.codurance.fizzbuzz;
 
 class FizzBuzz {
     String convert(int number) {
+        if (number % 3 == 0) return "Fizz";
         return String.valueOf(number);
     }
 }
--- src/test/java/com/codurance/fizzbuzz/FizzBuzzShould.java
+++ src/test/java/com/codurance/fizzbuzz/FizzBuzzShould.java
@@ -8,7 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class FizzBuzzShould {
     @ParameterizedTest
     @CsvSource({ "1,1", "2,2", "4,4" })
-    void convert_number_to_FizzBuzz_string(int input, String expectedOutput) {
+    void convert_regular_number_to_number_string(int input, String expectedOutput) {
+        assertEquals(expectedOutput, new FizzBuzz().convert(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "3,Fizz", "6,Fizz", "9,Fizz" })
+    void convert_multiples_of_3_to_Fizz(int input, String expectedOutput) {
         assertEquals(expectedOutput, new FizzBuzz().convert(input));
     }
 }
